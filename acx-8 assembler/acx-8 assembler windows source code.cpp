3646776ea8be4717804ce969f213fa0d7fc894af
amelia7772 <ameliashananya@gmail.com>
2026-07-28 00:16:12 +0300

    Update acx-8 assembler windows source code.cpp

--- acx-8 assembler/acx-8 assembler windows source code.cpp
+++ acx-8 assembler/acx-8 assembler windows source code.cpp
@@ -324,21 +324,68 @@ bool output_binary_code(int current_file_index) {
 }
 
 void deallocate_memory() {
-	delete code_text;
-	delete binary_output_code;
-	delete original_code_text;
-	delete temp_code_text;
-	delete current_line;
-	delete instruction_types;
-	delete temp_parameter;
-	delete labels;
-	delete temp_labels;
-	delete input_file_names;
-	delete macros_to_be_expanded;
-	//delete directives;
-	delete code_tokens;
-	delete expression_tokens;
-	delete byte_pointers;
+	if(code_text != nullptr){
+		delete code_text;
+		code_text = nullptr;
+	}
+	if(binary_output_code != nullptr){
+		delete binary_output_code;
+		binary_output_code = nullptr;
+	}
+	if(original_code_text != nullptr){
+		delete original_code_text;
+		original_code_text = nullptr;
+	}
+	if(temp_code_text != nullptr){
+		delete temp_code_text;
+		temp_code_text = nullptr;
+	}
+	if(current_line != nullptr){
+		delete current_line;
+		current_line = nullptr;
+	}
+	if(instruction_types != nullptr){
+		delete instruction_types;
+		instruction_types = nullptr;
+	}
+	if(temp_parameter != nullptr){
+		delete temp_parameter;
+		temp_parameter = nullptr;
+	}
+	if(labels != nullptr){
+		delete labels;
+		labels = nullptr;
+	}
+	if(temp_labels != nullptr){
+		delete temp_labels;
+		temp_labels = nullptr;
+	}
+	if(input_file_names != nullptr){
+		delete input_file_names;
+		input_file_names = nullptr;
+	}
+	if(macros_to_be_expanded != nullptr){
+		delete macros_to_be_expanded;
+		macros_to_be_expanded = nullptr;
+	}
+	/*
+	if(directives != nullptr){
+		delete directives;
+		directives = nullptr;
+	}
+	*/
+	if(code_tokens != nullptr){
+		delete code_tokens;
+		code_tokens = nullptr;
+	}
+	if(expression_tokens != nullptr){
+		delete expression_tokens;
+		expression_tokens = nullptr;
+	}
+	if(byte_pointers != nullptr){
+		delete byte_pointers;
+		byte_pointers = nullptr;
+	}
 }
 
 int get_line_of_index(int index) {
